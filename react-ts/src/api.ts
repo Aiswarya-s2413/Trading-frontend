48a0e83962f44b0353a83bd7b2a4142a6c633627
AISWARYA S <aiswarya@AISWARYAs-MacBook-Air.local>
2025-12-11 18:49:46 +0530

    52 weeks high updated

--- react-ts/src/api.ts
+++ react-ts/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api";
-// const API_BASE_URL = "https://trading.aiswaryasathyan.space/api";
+// const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://trading.aiswaryasathyan.space/api";
 
 export interface PriceData {
   time: number; // Unix timestamp
